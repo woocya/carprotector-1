e4b7ec982e01b2ef73bfff10ad73e047ee3ecfff
woocya <lwendland5@gmail.com>
2021-06-04 23:30:50 +0200

    First add

---
+++ src/main/java/com/protector/carprotector/CarProtectorApplication.java
@@ -0,0 +1,13 @@
+package com.protector.carprotector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarProtectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarProtectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protector/carprotector/CarProtectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protector.carprotector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarProtectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
